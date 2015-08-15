7aa9efa25547f82f999eb2f3e98fcae5717a6f05
Julien Vaubourg <julien@vaubourg.com>
2015-08-15 15:45:01 +0200

    Beautiful html page

--- olinux/script/initramfs/post.sh
+++ olinux/script/initramfs/post.sh
@@ -1,30 +1,17 @@
 #!/bin/sh
+
 read QUERY_STRING
-eval $(echo "$QUERY_STRING"|awk -F'&' '{for(i=1;i<=NF;i++){print $i}}')
-tmp=`httpd -d $Text_Field`
-echo -ne $tmp >/lib/cryptsetup/passfifo
-i=0
-while true; do
+eval $(echo "$QUERY_STRING" | awk -F'&' '{for(i=1; i <= NF; i++) { print $i }}')
+
+echo -n $(httpd -d $passphrase) > /lib/cryptsetup/passfifo
+
+for i in $(seq 10); do
   sleep 1
-  i=$(($i + 1))
   echo $i
-  if [ -f /dev/mapper/root ] ; then
-    echo "<html>"
-    echo "  <head>"
-    echo "    <title>Unlock root partition</title>"
-    echo "  </head>"
-    echo ""
-    echo "  <body>"
-    echo "    <div style='text-align: center;'><IMG SRC='../unicorn.gif' ALT='image'>"
-    echo "      Disk unlock !!"
-    echo "    </div>"
-    echo "  </body>"
-    echo "</html>"
-    exit 0
-  elif [ ${i} -gt 10 ] ; then
-    cat ../index.html
-    exit 0
-  fi
+
+  [ -f /dev/mapper/root ] && exit 0
 done
+
+cat index.html | sed '/TPL:ERROR/d'
 
 exit 0
