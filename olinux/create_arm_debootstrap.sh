3a996bb55f5d3c8dd3738c557d45508c545ce978
Émile Morel <github@bleuchtang.fr>
2015-08-18 09:47:38 +0200

    rename initramfs files

--- olinux/create_arm_debootstrap.sh
+++ olinux/create_arm_debootstrap.sh
@@ -275,11 +275,8 @@ EOT
       cp /olinux/script/initramfs/stunnel.conf $TARGET_DIR/etc/initramfs-tools/
       cp /olinux/script/initramfs/stunnel_start $TARGET_DIR/etc/initramfs-tools/scripts/local-top/stunnel
       cp /olinux/script/initramfs/stunnel_stop $TARGET_DIR/etc/initramfs-tools/scripts/local-bottom/stunnel
-      mkdir -p $TARGET_DIR/etc/initramfs-tools/root/www/cgi-bin
-      cp /olinux/script/initramfs/index.html $TARGET_DIR/etc/initramfs-tools/root/www/
-      cp /olinux/script/initramfs/unicorn.gif $TARGET_DIR/etc/initramfs-tools/root/www/
-      cp /olinux/script/initramfs/unlocked.gif $TARGET_DIR/etc/initramfs-tools/root/www/
-      cp /olinux/script/initramfs/post.sh $TARGET_DIR/etc/initramfs-tools/root/www/cgi-bin/
+      mkdir -p $TARGET_DIR/etc/initramfs-tools/root
+      cp /olinux/script/initramfs/www $TARGET_DIR/etc/initramfs-tools/root/
       chroot_deb $TARGET_DIR "update-initramfs -u -k all"
     fi
   else
