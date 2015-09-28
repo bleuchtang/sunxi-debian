79cd6521d683d92656ca792e7e2d6065d4198190
Émile Morel <github@bleuchtang.fr>
2015-09-28 19:51:45 +0200

    remove useless condition

--- olinux/create_device.sh
+++ olinux/create_device.sh
@@ -149,9 +149,7 @@ fi
 sync
 
 echo "- Write sunxi-with-spl"
-if [[ `file ${DEB_DIR} | grep 'DOS/MBR'` ]] ; then
   dd if=${UBOOT_FILE} of=${DEVICE} bs=1024 seek=8 >/dev/null 2>&1
-fi	  
 sync
 
 if [ "${DEVICE}" = "img" ] ; then
