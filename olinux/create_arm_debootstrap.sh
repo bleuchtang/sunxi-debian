52d5da94e412e15114fd561a4c7b847db397009d
Émile Morel <github@bleuchtang.fr>
2015-10-01 01:04:39 +0200

    change autoinstall yunohost call

--- olinux/create_arm_debootstrap.sh
+++ olinux/create_arm_debootstrap.sh
@@ -216,7 +216,7 @@ chroot_deb $TARGET_DIR "/bin/systemctl enable firstrun >> /dev/null"
 if [ $INSTALL_YUNOHOST ] ; then
   chroot_deb $TARGET_DIR "$APT git"
   chroot_deb $TARGET_DIR "git clone https://github.com/YunoHost/install_script /tmp/install_script"
-  chroot_deb $TARGET_DIR "cd /tmp/install_script && ./autoinstall_yunohostv2"
+  chroot_deb $TARGET_DIR "cd /tmp/install_script && ./install_yunohostv2 -a"
 fi
 
 if [ $INSTALL_KERNEL ] ; then
